61c4d97a482286629a6c6cfa230d19a796c1841d
Rahul G <rahulstark13@gmail.com>
2024-05-07 10:34:10 +0530

    fetched user details and updated profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     implementation("androidx.activity:activity:1.8.0")
     implementation("com.google.firebase:firebase-database-ktx:20.3.1")
     implementation("com.google.firebase:firebase-messaging-ktx:24.0.0")
+    implementation("com.google.firebase:firebase-firestore-ktx:25.0.0")
+    implementation("com.google.firebase:firebase-storage-ktx:21.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
